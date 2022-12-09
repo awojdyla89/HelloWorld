b48dfd23beed78f133590debb2a789101b85fc09
Adam Wojdyla <awojdyla@gmail.com>
2022-12-09 12:58:41 -0800

    First Commit

---
+++ src/tech/strategio/helloworld/HelloWorldClass.java
@@ -0,0 +1,9 @@
+package tech.strategio.helloworld;
+
+public class HelloWorldClass {
+    public static void main(String[] args) {
+        //TODOd: implement hello world
+        System.out.println("Hello World");
+
+    }
+}
